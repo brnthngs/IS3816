f7587db698cae7f7774bc2adac8b5fad3865d397
Benjamin <Benjamin@Jezabelle>
2016-01-28 21:22:33 -0600

    Merge origin/master
    Conflicts:
    	.gitignore

--- MyPlayground/src/edu/umsl/Student.java
+++ MyPlayground/src/edu/umsl/Student.java
@@ -6,29 +6,40 @@
 
 package edu.umsl;
 
+import java.util.Scanner;
+
 /*
- 
+
   @author lawtonb
  */
 
+/*
 
+*/
 public class Student
 {
-   private String fname;
-   private String lname;
-   private int exam1;
-   private int exam2;
-   private double studentAvg;
-   
-   public void setfname(String fname)
-   {
-       this.fname = fname;
-   }
-   
-   public String getfname( )
-   {
-       return fname;
-   }
+    
+    Scanner sc = new Scanner(System.in);
+    private String fname;
+    private String lname;
+    private int exam1;
+    private int exam2;
+    private double studentAvg;
+
+    /*
+
+    */
+    public void setfname(String fname)
+    {
+        this.fname = fname;
+    }
+    /*
+
+    */
+    public String getfname( )
+    {
+        return fname;
+    }
 
     public String getLname() 
     {
@@ -59,7 +70,7 @@ public class Student
     {
         this.exam2 = exam2;
     }
-    
+
     private void calcAvg ()
     {
         studentAvg = (exam1 + exam2)/2.0;
@@ -73,7 +84,7 @@ public class Student
         calcAvg();
         return studentAvg;
     }
-    
+
     public void showStudent()
     {
         System.out.println("First Name: " + fname);
@@ -81,11 +92,88 @@ public class Student
         System.out.println("Exam 1: " + exam1);
         System.out.println("Exam 2: " + exam2);
         System.out.println("Student Average: " + getStudentAvg());
+        calcLetterGrade();
     }
-    
+
     public void calcLetterGrade()
     {
-        
+        if (studentAvg >=80)
+        {
+            // Convert double to int to use the switch case statement
+            double x = studentAvg;
+            int stdAvg = (int)x;
+            switch (stdAvg) 
+            {
+                case 100:
+                case 99:
+                case 98:
+                case 97:
+                case 96:
+                case 95:
+                case 94:
+                case 93:
+                case 92:
+                case 91:
+                case 90:
+                    System.out.println("Your letter grade is an A");
+                    break;
+                case 89:
+                case 88:
+                case 87:
+                case 86:
+                case 85:
+                case 84:
+                case 83:
+                case 82:
+                case 81:
+                case 80:
+                    System.out.println("Your letter grade is a B");
+                    break;
+                default:
+                    System.out.println("Invalid grade, please enter a number "
+                            + "between 0 - 100");
+            }
+        }
+            else 
+                {
+                    if (studentAvg > 70)
+                    {
+                        System.out.println("Your letter grade is a C");
+                    }
+                    else if (studentAvg > 60)
+                    {
+                        System.out.println("Your letter grade is a D");
+                    }
+                    else 
+                    {
+                        System.out.println("Your letter grade is an F");
+                    }
+                }
+
+
+
+
+//        if(input>=90){
+//                sumA++;
+//                System.out.println(new Integer(input) + "      A");
+//			}
+//            else if(input >= 70){
+//                sumB++;
+//                System.out.println(new Integer(input) + "      B");
+//            }
+//            else if(input >= 50){
+//                sumC++;
+//                System.out.println(new Integer(input) + "      C");
+//            }
+//            else if(input >= 35){
+//                sumD++;
+//                System.out.println(new Integer(input) + "      D");
+//            }
+//            else{
+//                sumF++;
+//                System.out.println(new Integer(input) + "      F");
+//            }
+//
+//            input = kbd.readInt();
+        }
     }
-    
-}
