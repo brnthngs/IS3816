109b94b8b27263e3a62daa7beb2f00563d11900a
brnthngs <Benjamin@Jezabelle>
2016-02-23 00:20:19 -0600

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ IS3816/src/edu/umsl/ATM/Account.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.umsl.ATM;
+
+/**
+ *
+ * @author Benjamin
+ */
+
+/*
+ Assignment 1 - Bank ATM using Inheritance - March 16
+
+Posted on: Wednesday, February 17, 2016 7:19:20 PM CST
+
+    For this assignment you will use the puzzle pieces provided posted under 
+Docs & Assignments. 
+    The general code is given to you in the Account Class. 
+    The other pieces show a students class and its sub classes 
+(inherited/extended class) of Grad and UnderGrad students along with another 
+class called RunStudent, Process Student or StudentArray
+    You will need use the model provided with the student class and build a 
+model for a Bank ATM
+    The bank ATM must allow for three unique accounts in an array, arraylist or 
+hashmap collection
+    You should be allowed to select an account and then perform deposits, 
+withdraws and check balances on the account selected
+    Each account should be either a checking or savings account that inherits 
+from the bank account class whether a class is checking or savings will 
+determine via inheritance how much interest is calculated for each transaction
+    The dates used to calculate interest are in the account code given to you, 
+you will have to enter dates for each transaction to determine the interest this 
+is evident in the account code given to you.  You will only need to assume that 
+we are entering dates for the current year.
+    Additionally you will need to have your program write and read from a file 
+where you will write out your array of objects or you can write out in plain 
+text if you want to process line by line.
+
+Good Luck!
+*/
+
+
+public class Account {
+    
+}
